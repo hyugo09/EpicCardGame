d2115ef045ef47b0f167f2e0eec35b3305d0f418
hyugo09 <hugo.lavoietalbot@gmail.com>
2024-03-08 14:25:08 -0500

    fait button pour phase

--- Assets/Scripts/Script pour carte/CardGameManager.cs
+++ Assets/Scripts/Script pour carte/CardGameManager.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CardGameManager : MonoBehaviour
 {
@@ -13,7 +15,8 @@ public class CardGameManager : MonoBehaviour
         end,
         ennemy,
     }
-
+    [SerializeField] private Button button;
+    [SerializeField] private TextMeshProUGUI text;
     public GameObject selected = null;
     [SerializeField] internal Phase currentPhase;
     internal Vector3 originalposSelected;
@@ -32,14 +35,18 @@ public class CardGameManager : MonoBehaviour
     }
     private void Awake()
     {
-       
+        text = button.GetComponentInChildren<TextMeshProUGUI>();
+        text.text = "omegalol";
     }
     // Update is called once per frame
     void Update()
     {
 
     }
-
+    private void OnMouseDown()
+    {
+        ChangeCurrentPhase();
+    }
     public void ChangeCurrentPhase()
     {
         switch (currentPhase)
@@ -75,15 +82,18 @@ public class CardGameManager : MonoBehaviour
     private void ChangeToDraw()
     {
         currentPhase = Phase.draw;
+        text.text = "Draw Phase";
         DrawCard();
         ChangeCurrentPhase();
     }
     private void ChangeToMain()
     {
-        
+
         currentPhase = Phase.main;
+        text.text = "Main Phase";
         if (isAi)
         {
+            text.text = "Ennemie Main";
             Playcard();
             Playcard();
             ChangeCurrentPhase();
@@ -92,10 +102,12 @@ public class CardGameManager : MonoBehaviour
     private void ChangeToBattle()
     {
         currentPhase = Phase.battle;
+        text.text = "battle phase";
     }
     private void ChangeToEnd()
     {
         currentPhase = Phase.end;
+        text.text = "End Phase";
         enemyGameManager.ChangeCurrentPhase();
         ChangeCurrentPhase();
     }
@@ -119,31 +131,32 @@ public class CardGameManager : MonoBehaviour
     }
     private void Playcard()
     {
-        
-        
+
+
         if (playerHand.cards.Count > 0)
         {
             GameObject temp = playerHand.cards[Random.Range(0, playerHand.cards.Count)];
-            if (temp != null) { 
-                Field ftemp = allField[Random.Range(0,allField.Length)];
+            if (temp != null)
+            {
+                Field ftemp = allField[Random.Range(0, allField.Length)];
                 if (ftemp.carteSurField == null)// ajouter la condition pour voir si c jouable
                 {
                     //choisir un field et faire passer le meme truc de mouse down
-                    
+
                     ftemp.JouerCarte(temp);
-                    
+
                 }
                 else
                 {
                     Playcard();
                 }
             }
-            
-            
+
+
         }
     }
     private void AiAttack(int layerIndex)
     {
-        
+
     }
 }
--- Assets/Scripts/cam et déplacement/Battle Start Camera Controller.cs
+++ Assets/Scripts/cam et déplacement/Battle Start Camera Controller.cs
@@ -7,21 +7,33 @@ public class BattleStartCameraController : MonoBehaviour
 {
     [SerializeField] private CinemachineVirtualCamera firstCamera;
     [SerializeField] private CinemachineVirtualCamera battleCamera;
+    [SerializeField] private GameObject button;
+    
     void Start()
     {
         firstCamera.Priority = 1;
         battleCamera.Priority = 0;
+        button.SetActive(false);
+        StartCoroutine(changementCamera(0.01f));
     }
 
     // Update is called once per frame
     void Update()
     {
-        StartCoroutine(changementCamera(0.01f));
+        
     }
     IEnumerator changementCamera(float delais)
     {
         yield return new WaitForSeconds(delais);
         firstCamera.Priority = 0;
         battleCamera.Priority = 1;
+        StartCoroutine(ActivateUI(3.8f));
+        
+    }
+    public IEnumerator ActivateUI(float delais)
+    {
+        yield return new WaitForSeconds (delais);
+        button.SetActive (true);
+        
     }
 }
