efe3243bc4615c70ea152e25eb6e061deaa7e6ff
minigungold <122937479+minigungold@users.noreply.github.com>
2024-03-15 15:09:24 -0400

    Jacob
    update de la semaine

--- Assets/Scripts/Script pour carte/Card.cs
+++ Assets/Scripts/Script pour carte/Card.cs
@@ -15,7 +15,6 @@ public class Card : MonoBehaviour
     public bool cardonfield = false;
     public int[] direction = {3};
     public float width;
-    public bool canAttack;
 
     // Start is called before the first frame update
     void Start()
