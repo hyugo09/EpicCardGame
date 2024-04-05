2f797226ac4ce7c6a0cc804ebc7b2602f1f3cd08
Stevens <122079885+beokki@users.noreply.github.com>
2024-04-05 17:46:55 -0400

    Save n Load
    devrait save la scene + position du joueur // issue: position not loaded

---
+++ Assets/Scripts/SN/GameData.cs
@@ -0,0 +1,10 @@
+[System.Serializable]
+
+public class GameData
+{
+    public float playerPositionX;
+    public float playerPositionY;
+    public float playerPositionZ;
+    public float playerRotationY;
+    public string currentSceneName;
+}
