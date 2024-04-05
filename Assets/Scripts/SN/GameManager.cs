17d716317fbf21854722dd7e0c00acb156d42ead
Stevens <122079885+beokki@users.noreply.github.com>
2024-04-05 17:48:55 -0400

    Revert "Save n Load"
    This reverts commit 2f797226ac4ce7c6a0cc804ebc7b2602f1f3cd08.

--- Assets/Scripts/SN/GameManager.cs
+++ Assets/Scripts/SN/GameManager.cs
@@ -1,6 +1,5 @@
 using Cinemachine;
 using System.Collections;
-using System.IO;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -8,9 +7,6 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     public GameObject pauseUI;
-    public GameObject player;
-    public GameData currentGameData;
-
     Scene currentScene;
 
     private void FixedUpdate()
@@ -101,75 +97,4 @@ public class GameManager : MonoBehaviour
     }
 
     public bool isPaused { get; private set; } = false;
-
-    public void SaveGame()
-    {
-        if (player == null) return; // Ensure there is a player to save
-
-        currentGameData.currentSceneName = SceneManager.GetActiveScene().name;
-        currentGameData.playerPositionX = player.transform.position.x;
-        currentGameData.playerPositionY = player.transform.position.y;
-        currentGameData.playerPositionZ = player.transform.position.z;
-        currentGameData.playerRotationY = player.transform.eulerAngles.y;
-
-        try
-        {
-            string path = Application.persistentDataPath + "/savefile.json";
-            string json = JsonUtility.ToJson(currentGameData, true);
-            File.WriteAllText(path, json);
-            Debug.Log("Game saved successfully to: " + path);
-        }
-        catch (System.Exception ex)
-        {
-            Debug.LogError("Save failed: " + ex.Message);
-        }
-    }
-
-    public void LoadGame()
-    {
-        string path = Application.persistentDataPath + "/savefile.json";
-        if (File.Exists(path))
-        {
-            string json = File.ReadAllText(path);
-            currentGameData = JsonUtility.FromJson<GameData>(json);
-
-            SceneManager.LoadScene(currentGameData.currentSceneName);
-        }
-        else
-        {
-            Debug.LogError("Save file not found in " + path);
-        }
-    }
-
-    private void OnEnable()
-    {
-        SceneManager.sceneLoaded += OnSceneLoaded;
-    }
-
-    private void OnDisable()
-    {
-        SceneManager.sceneLoaded -= OnSceneLoaded;
-    }
-
-    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
-    {
-        player = GameObject.FindGameObjectWithTag("Player");
-        if (scene.name == currentGameData.currentSceneName && player != null)
-        {
-            ApplySaveGame();
-        }
-    }
-
-    private void ApplySaveGame()
-    {
-        Rigidbody playerRb = player.GetComponent<Rigidbody>();
-        Vector3 loadedPosition = new Vector3(currentGameData.playerPositionX, player.transform.position.y, currentGameData.playerPositionZ);
-        if (playerRb != null)
-        {
-            bool wasKinematic = playerRb.isKinematic;
-            playerRb.isKinematic = true; // Temporarily disable physics interactions
-            playerRb.position = loadedPosition;
-            playerRb.isKinematic = wasKinematic;
-        }
-    }
 }
